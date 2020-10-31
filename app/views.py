785a514da322f2bc2e0c8cf30781f21d13741fa7
firas <firas.abu.sneneh@hotmail.com>
2020-10-31 07:36:53 +0200

    first commit

---
+++ app/views.py
@@ -0,0 +1,50 @@
+import flask
+
+from . import app 
+from . import forms
+from .models import Product
+from . import db
+
+def add_Products():
+    
+    product =Product(name="Macbook Pro", price=1000.99, quantity=3,category="technology")
+    db.session.add(product)
+    db.session.commit()
+
+    product =Product(name="Dell Laptop", price=899.99, quantity=5,category="technology")
+    db.session.add(product)
+    db.session.commit()
+
+
+    product =Product(name="Brita Water filter", price=39.00, quantity=2,category="kitchen")
+    db.session.add(product)
+    db.session.commit()
+
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return flask.render_template("index.html", title="My awesome app", title2="Awesome app")
+
+@app.route("/product/<product_id>")
+def filter_product_by_id(product_id):
+
+    for product in Product.query.all():
+        if product.id == int(product_id):
+            break
+    else:
+        return "Product not found"
+
+    return flask.render_template("product_page.html", 
+    product_name = product.name ,
+    product_price =  product.price , 
+    product_stock =  product.quantity , product_category = product.category)
+
+@app.route("/search/by-category/<category>")
+def search_product_by_category(category):
+    category_products = []
+    for product in Product.query.all():
+        if product.category == category :
+            category_products.append(product.name)
+        
+    return flask.render_template("products_list.html" , filtred_products = category_products)
